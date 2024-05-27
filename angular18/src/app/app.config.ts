3ca4625f52946385f18a37c83f8142fee54f62f8
honnappapujari <pujarihonnappa888@gmail.com>
2024-05-27 20:58:33 +0530

    #

--- angular18/src/app/app.config.ts
+++ angular18/src/app/app.config.ts
@@ -3,7 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"angular-fde6b","appId":"1:807562902176:web:16be4fab4198568f2127c5","databaseURL":"https://angular-fde6b-default-rtdb.firebaseio.com","storageBucket":"angular-fde6b.appspot.com","apiKey":"AIzaSyC5QTViGl2rk6SMSsld2Jk8qH_4ULgA2dY","authDomain":"angular-fde6b.firebaseapp.com","messagingSenderId":"807562902176"})), provideFirestore(() => getFirestore())]
 };
--- angular18/src/app/pages/login/login.component.ts
+++ angular18/src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Firestore, collection,addDoc, collectionData, doc, updateDoc } from '@angular/fire/firestore';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
@@ -10,8 +11,9 @@ import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } 
 })
 export class LoginComponent {
   form : FormGroup;
-  constructor(private fb: FormBuilder)
+  constructor(private fb: FormBuilder, private firestore : Firestore)
   {
+    this.getData();
     this.form = this.fb.group(
       {
         email: new FormControl('', [Validators.required]),
@@ -20,11 +22,42 @@ export class LoginComponent {
       }
     )
   }
-  onSubmit()
+  async onSubmit()
   {
     if(this.form.valid)
     {
-      console.log(this.form.value)
+      console.log(this.form.value);
+      const connectionInstance = collection(this.firestore, 'users');
+     addDoc(connectionInstance, this.form.value).then(() =>
+     {
+        console.log('Login successfull');
+     }).catch((err) =>
+     {
+      console.log('Error',err)
+     })
+        }
+  }
+  getData()
+  {
+    const collectionInstance = collection(this.firestore, 'users');
+    collectionData(collectionInstance).subscribe( (val: any) =>
+    {
+      console.log(val)
+    })
+  }
+
+  updateData(id:string)
+  {
+    const docInstance = doc(this.firestore, 'users', id);
+    const updateData = {
+      name: 'updatedName'
     }
+    updateDoc(docInstance, updateData).then(()=>
+    {
+      console.log('succefully Updated')
+    }).catch((err) =>
+    {
+      console.log(err)
+    })
   }
 }
